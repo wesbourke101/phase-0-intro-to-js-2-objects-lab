583050354d70be1452584f2f5f831befd35d57ec
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-02 16:18:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Wes",
+    streetAddress: "9 Pennsylvania ave",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     return employee = {
+//         ...employee, 
+//         [key]: value,
+//     };
+// };
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+}
