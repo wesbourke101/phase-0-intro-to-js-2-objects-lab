2ea3e7da4fd6648e538c9d3c7ced6e420d3d6f38
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-02 18:26:15 -0600

    Done.

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
     };
 };
 
-// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-//     return employee = {
-//         employee, 
-//         [key]: value,
-//     };
-// };
-
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
 
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {employee};
+    delete newEmployee.key;
+    return newEmployee;
+}
 
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.key;
+    //return newEmployee;
+    return employee;
+}
