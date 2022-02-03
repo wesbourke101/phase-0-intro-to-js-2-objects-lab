2c0989498a59e7f59ff37826966fc81b1dda2479
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-02 18:38:40 -0600

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromEmployeeByKey(employee, key) {
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee.key;
+    delete employee[key];
     return employee;
 }
